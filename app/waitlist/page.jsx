05fb0002d6ceae1719d975a9d6c7796afc7ea34a
Danuar Ihza Mahendra <danuarim09@gmail.com>
2025-05-17 21:17:52 +0700

    deploy

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WaitlistPage = () => {
+  return (
+    <div>
+      <div>
+        <iframe style={{width: "100%;"}} height="730" src="https://werlos-rentall.created.app" title="Waiting List Landing Page" frameBorder="0"></iframe>
+        
+      </div>
+    </div>
+  );
+};
+
+export default WaitlistPage;
